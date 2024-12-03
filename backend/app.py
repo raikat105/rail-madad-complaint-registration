ac840f8c1e95014cd1ebdd88e6729cdb3e9daea9
raikat105 <raikatghosh105@gmail.com>
2024-12-03 16:23:37 +0530

    fixed

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app) 
 
 @app.route('/upload', methods=['POST'])
 def handle_upload():
